e129d25c7d24c05a3737a659985e34966f0ad1e4
FullyRobert <46491041+FullyRobert@users.noreply.github.com>
2022-01-13 16:42:23 +0800

    use w_naf to reduce time consume of point_mul (#30)
    * use shr to replace div 2 for inv_n
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * use w_naf to reduce time consume of point_mul
    
    * use clippy
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    * Update src/sm2/ecc.rs
    
    Co-authored-by: Yieazy <yuitta@163.com>
    
    Co-authored-by: Yieazy <yuitta@163.com>

--- src/sm2/ecc.rs
+++ src/sm2/ecc.rs
@@ -25,7 +25,7 @@ pub struct EccCtx {
     n: BigUint,
 }
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, Copy)]
 pub struct Point {
     pub x: FieldElem,
     pub y: FieldElem,
@@ -379,7 +379,83 @@ impl EccCtx {
 
         let k = FieldElem::from_biguint(&m);
 
-        self.mul_raw(&k.value, p)
+        self.mul_raw_naf(&k.value, p)
+    }
+
+    pub fn w_naf(&self, m: &[u32], w: usize, lst: &mut usize) -> [i8; 257]{
+        let mut carry = 0;
+        let mut bit = 0;
+        let mut ret: [i8; 257] = [0; 257];
+        let mut n:[u32; 9] = [0; 9];
+
+        n[1..9].clone_from_slice(&m[..8]);
+
+        let window: u32 = (1 << w) -1;
+
+        while bit < 256 {
+            let u32_idx = 8 - bit as usize / 32;
+            let bit_idx = 31 - bit as usize % 32;
+            let mut word: u32 ;
+
+            if ((n[u32_idx] >> (31 - bit_idx)) & 1) == carry{
+                bit += 1;
+                continue;
+            }
+
+            if bit_idx >= w-1 {
+                word = (n[u32_idx] >> (31 - bit_idx)) & window;
+            }
+            else{
+                word = ((n[u32_idx] >> (31 - bit_idx)) | (n[u32_idx - 1] << (bit_idx + 1)) ) & window;
+            }
+
+            word += carry;
+
+            carry = (word >> (w - 1)) & 1;
+            ret[bit] = word as i8 - (carry << w) as i8;
+
+            *lst = bit;
+            bit += w;
+
+        }
+
+        if carry == 1{
+            ret[256] = 1;
+            *lst = 256;
+        }
+
+        ret
+    }
+
+    pub fn mul_raw_naf(&self, m: &[u32], p: &Point) -> Point {
+        let mut i = 256;
+        let mut q = self.zero();
+        let naf = self.w_naf(&m,5, &mut i);
+        let offset = 16;
+        let mut table = [self.zero(); 32];
+        let double_p = self.double(&p);
+
+        table[1 + offset] = p.clone();
+        table[offset - 1] = self.neg(&table[1 + offset]);
+        for i in 1..8 {
+            table[2 * i + offset + 1] = self.add(&double_p, &table[2 * i + offset -1]);
+            table[offset - 2 * i - 1] = self.neg(&table[2 * i + offset + 1]);
+        }
+
+        loop {
+            q = self.double(&q);
+
+            if naf[i] != 0 {
+                let index = (naf[i] + 16) as usize;
+                q = self.add(&q, &table[index]);
+            }
+
+            if i == 0 {
+                break;
+            }
+            i -= 1;
+        }
+        q
     }
 
     pub fn mul_raw(&self, m: &[u32], p: &Point) -> Point {
@@ -634,6 +710,38 @@ mod tests {
         let new_g = curve.g_mul(&n);
         let nn_g = curve.mul(&n, &g);
         assert!(curve.eq(&nn_g, &new_g));
+    }
+
+    #[test]
+    fn test_w_naf() {
+        let curve = EccCtx::new();
+        let mut lst = 0;
+
+        let n = curve.get_n() - BigUint::one();
+        let _num = BigUint::from(1122334455 as u32) - BigUint::one();
+
+        let k = FieldElem::from_biguint(&n);
+        let ret = curve.w_naf(&k.value,5, &mut lst);
+        let mut sum = BigUint::zero();
+        let mut init = BigUint::from_str_radix(
+            "10000000000000000000000000000000000000000000000000000000000000000",
+            16,
+        ).unwrap();
+
+        for j in 0..257{
+            let i = 256 - j;
+            if ret[i] != 0{
+                if ret[i] > 0{
+                    sum += &init * BigUint::from(ret[i] as u8);
+                }
+                else{
+                    let neg = (0 - ret[i]) as u8;
+                    sum -= &init * BigUint::from(neg as u8);
+                }
+            }
+            init = init >> 1;
+        }
+        assert_eq!(sum, n);
     }
 
     #[test]
