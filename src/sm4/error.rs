91530790abdd8cc9a43438a10edb95675a79c79e
hujiang <a15174027322@icloud.com>
2022-05-09 17:59:22 +0800

    refactor: fix panic
    refactor: fix for format

---
+++ src/sm4/error.rs
@@ -0,0 +1,57 @@
+// Copyright 2018 Cryptape Technology LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::error;
+use std::fmt::Display;
+use std::fmt::Formatter;
+
+pub enum Sm4Error {
+    ErrorBlockSize,
+}
+
+impl ::std::fmt::Debug for Sm4Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> ::std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl From<Sm4Error> for &str {
+    fn from(e: Sm4Error) -> Self {
+        match e {
+            Sm4Error::ErrorBlockSize => "the block size of SM4 must be 16",
+        }
+    }
+}
+
+impl Display for Sm4Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let err_msg = match self {
+            Sm4Error::ErrorBlockSize => "the block size of SM4 must be 16",
+        };
+        write!(f, "{}", err_msg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Sm4Error;
+
+    #[test]
+    fn test_error_display() {
+        assert_eq!(
+            format!("{}", Sm4Error::ErrorBlockSize),
+            "the block size of SM4 must be 16"
+        )
+    }
+}
